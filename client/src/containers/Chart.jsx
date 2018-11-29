0fc0e2fe774b7855891fd9f8cb7b8cd24cae3de1
pikajen <jennylam7@hotmail.com>
2018-11-28 17:33:38 -0800

    Implemented the x and y axis for the chart
    Using hardcoded values for the y axis for now

--- client/src/containers/Chart.jsx
+++ client/src/containers/Chart.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { GradientDarkgreenGreen } from '@vx/gradient';
+import { Bar } from '@vx/shape';
+import { AxisBottom, AxisLeft} from '@vx/axis';
+import {scaleLinear, scaleBand} from '@vx/scale';
 
 class Chart extends Component {
   state = {
@@ -29,7 +32,21 @@ class Chart extends Component {
   }
 
   render() {
+    const margin = 30;
     const { width, height } = this.state;
+    
+    const yScale = scaleLinear({
+    rangeRound: [height - (margin*2), 0],
+    domain: [0, 13],
+    nice:true
+    })
+
+    const xScale = scaleBand({
+      rangeRound: [0, width-(margin*2)],
+      padding: 0.2,
+      domain:["0m - 10m", "10m - 20m","20m - 30m", "30m - 40m","40m - 50m","50m - 60m","60m - 70m"],
+      align: 0.5
+    })
     
     /* This is a hack to first set the size based on percentage
        then query for the size so the chart can be scaled to the window size.
@@ -47,7 +64,39 @@ class Chart extends Component {
           width={width}
           height={height}
           fill={`url(#gradient)`}
+        />{
+        // <Bar
+        //   width={200}
+        //   height={400}
+        //   x={30}
+        //   y={60}
+        //   stroke={'black'}
+        // />
+      }
+        <AxisLeft
+          scale={yScale}
+          top={margin}
+          left={margin}
+          numTicks={14}
+          hideAxisLine
+          hideTicks
+          tickLabelProps={(value, index) => ({
+            fill: 'white',
+            fontSize: 11,
+          })}
         />
+        <AxisBottom
+            top={height-margin}
+            left={margin}
+            scale={xScale}
+            hideAxisLine
+            hideTicks
+            tickLabelProps={(value, index) => ({
+              fill: 'white',
+              fontSize: 11,
+              textAnchor: 'middle'
+            })}
+          />
       </svg>
     );
   }
